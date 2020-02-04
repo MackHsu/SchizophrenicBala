9892aa9dcb442a7890966459e1ae9a890fd61c09
MackHsu <786188853@qq.com>
2020-02-04 23:25:43 +0800

    添加了人格json和剧情json的模版

---
+++ Assets/Scripts/C# Code/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LitJson;
+using System.IO;
+using System.Text;
+
+
+public class GameManager : MonoBehaviour
+{
+
+    int personalityId = 0;
+    int textId = 0;
+    private JsonData personalities;
+    JsonData textJson;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        personalities = JsonMapper.ToObject(File.ReadAllText(Application.dataPath + "/Scripts/json/personalitiesTest.json", Encoding.GetEncoding("GB2312")));
+        textJson = JsonMapper.ToObject(File.ReadAllText(Application.dataPath + "/Scripts/json/textTest.json", Encoding.GetEncoding("GB2312")));
+        string pName = "bǎlà";
+        for(int i = 0; i < personalities.Count; i++)
+        {
+            if((int)personalities[i]["pid"] == personalityId)
+            {
+                pName = (string)personalities[i]["name"];
+                break;
+            }
+        }
+        int textType = (int)textJson[textId]["textType"];
+        string text = "";
+        //对话
+        if (textType == 0)
+        {
+            text = (string)textJson[textId]["content"][0]["content"];
+        }
+        //剧情文本或提示文本
+        else
+        {
+            text = (string)textJson[textId]["content"];
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/C# Code/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+    float horizontalMove = 0f;
+    public float moveSpeed = 100;
+    Transform playerTransform;
+    Transform groundTransfrom;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GameObject.Find("Player").GetComponent<Transform>();
+        groundTransfrom = GameObject.Find("Ground").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * moveSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+        if (playerTransform.position.x < groundTransfrom.position.x - groundTransfrom.lossyScale.x / 2)
+        {
+            playerTransform.position = new Vector3(groundTransfrom.position.x - groundTransfrom.lossyScale.x / 2, playerTransform.position.y, playerTransform.position.z);
+        }
+    }
+}
