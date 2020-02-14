7128774c44f0c523dc5e3358000871fda2a54d18
MackHsu <786188853@qq.com>
2020-02-14 18:28:29 +0800

    写好了存档功能，读档功能未写

--- Assets/Scripts/C# Code/SaveSlots.cs
+++ Assets/Scripts/C# Code/SaveSlots.cs
@@ -10,27 +10,35 @@ public class SaveSlots : MonoBehaviour
     public GameObject slot1;
     public GameObject slot2;
     public GameObject slot3;
+    int selectedId = 0;
 
     public Button confirm;
     public Button cancel;
     // Start is called before the first frame update
     void Start()
     {
-        confirm.onClick.AddListener(Confirm);
         cancel.onClick.AddListener(Cancel);
         LoadDatas(slot1, 1);
         LoadDatas(slot2, 2);
         LoadDatas(slot3, 3);
+        confirm.onClick.AddListener(Save);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        if (selectedId < 1 || selectedId > 3) confirm.interactable = false;
+        else confirm.interactable = true;
 
-    void Confirm()
-    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray,out hit))
+            {
+                GameObject target = hit.collider.gameObject;
+            }
+        }
 
     }
 
@@ -58,6 +66,41 @@ public class SaveSlots : MonoBehaviour
                 }
             }
             texts.transform.Find("Personality").GetComponent<Text>().text = personality;
+        }
+    }
+
+    void Save()
+    {
+        using (StreamWriter sw = new StreamWriter(new FileStream("Save/save" + selectedId + ".json", FileMode.Truncate)))
+        {
+            string saveStr = JsonMapper.ToJson(GameManager.save);
+            sw.Write(saveStr);
+            sw.Close();
+        }
+        LoadDatas(slot1, 1);
+        LoadDatas(slot2, 2);
+        LoadDatas(slot3, 3);
+    }
+
+    public void Click(int slotId)
+    {
+        selectedId = slotId;
+        slot1.GetComponent<Image>().color = Color.white;
+        slot2.GetComponent<Image>().color = Color.white;
+        slot3.GetComponent<Image>().color = Color.white;
+        switch (selectedId)
+        {
+            case 1:
+                slot1.GetComponent<Image>().color = Color.gray;
+                break;
+            case 2:
+                slot2.GetComponent<Image>().color = Color.gray;
+                break;
+            case 3:
+                slot3.GetComponent<Image>().color = Color.gray;
+                break;
+            default:
+                break;
         }
     }
 }
